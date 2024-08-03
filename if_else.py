9dbd0480e550103143791d1fa037db0c0f1b1c08
yash dollarawar <yashdollarwar11@gmail.com>
2024-08-03 12:16:12 +0530

    if_else Function
    I have Explain if Else Function here.

---
+++ if_else.py
@@ -0,0 +1,14 @@
+#if else or elief
+num=(int(input("enter the value of num :="))) #-9
+
+if num>=0: #
+	if num%2==0:
+		print("the value is even:=")
+	else:
+		print("the value is odd=:")
+else:
+	print("it is invalid=:")
+
+##
+
+
