04d4b6f7d3a9ce1417e6b1b76e36436e314d25c2
yash dollarawar <yashdollarwar11@gmail.com>
2024-08-03 12:26:31 +0530

    Object Function

---
+++ object.py
@@ -0,0 +1,11 @@
+#class is group which having common properties and behaviour
+#object is the individual member of the class which having common physical proprties
+#and common physical behaviour
+
+For Ex:-
+def dance(self):
+	print("person dance")
+
+	#remember the class type of bird,animal and person
+
+#constructor:-
