c023ad33d6ab9574c47afc9bd8ddb0267046a6bc
yash dollarawar <yashdollarwar11@gmail.com>
2024-08-03 11:42:21 +0530

    Tuple data Type
    I have Explain The Tuple Here.

---
+++ tuple.py
@@ -0,0 +1,21 @@
+# list vs tupple(immutable)
+#insertion order preserved
+#allow hetrogenious
+# allow duplicates
+#index/slicing allow
+t=(10,20,30,"soft",True,10.5,10+20j,10,20,30)
+print(t[0])
+print(t[-1])
+print(t[0:5])
+#t.append
+print(type(t))
+print(t)
+
+
+###, is important to recognize tuple it represent the tuple value
+t=(10,)
+print(type(t))
+
+t1=10,
+print(type(t1))
+
