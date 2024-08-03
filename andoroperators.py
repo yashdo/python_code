5cdf6fada152895ca36e2bea2ec9cfd275dfeb46
yash dollarawar <yashdollarwar11@gmail.com>
2024-08-03 11:47:06 +0530

    And And Or Operator Explaition
    Here I have explain The work Of and  & Or Operator in Brief

---
+++ andoroperators.py
@@ -0,0 +1,36 @@
+#logical operators
+##and operators
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+###or operators
+print(True or True)
+print(True  or False)
+print(False or True)
+print(False or False)
+
+#1 & 1
+#0 & 1
+#1 & 0
+#0 & 0
+
+
+#1 | 1
+#0 | 1
+#1 | 0
+#0 | 0
+
+
+#1 ^ 1
+#0 ^ 1
+#1 ^ 0
+#0 ^ 0
+
+print(7 & 8)
+print(7 | 8)
+print(7 ^ 8)
+
+
+
+
