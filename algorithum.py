08f11be366c73f89ba11a1dec5fa6e83e0f6a980
yash dollarawar <yashdollarwar11@gmail.com>
2024-08-03 11:54:10 +0530

    algorithums
    Explaining Algorithums

---
+++ algorithum.py
@@ -0,0 +1,12 @@
+#algorithm :Wap For addition of the three number and input take from user
+#step1:-take a input from user and store 1st value to 1st variable
+#step2:-take a input from user and store 2nd value to 2nd variable
+#step3:-take a input from user and store 3rd value to 3rd variable
+#step4:-take a input from user and store 4th value to 4th variable
+       #logic: a+b+c
+	   #step5:-return a result and shoe it
+	   #implimentation
+	   
+#Wap For swaping two number by without using third variable
+
+
