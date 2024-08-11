661d2967253045f53e8a0a2245b4b0c61a326c53
yash dollarawar <yashdollarwar11@gmail.com>
2024-08-11 10:13:59 +0530

    mathamatical code

---
+++ square.py
@@ -0,0 +1,10 @@
+num=int(input("enter the value of num"))
+def square(num):
+	n =num**2
+	print(n)
+square(num)
+print("============Lamda Function===============")
+s=lambda num:num*num
+print(s(5)) 
+
+
