1d349129c5ce74a96c6b8c4dca013d66bcbd2cba
yash dollarawar <yashdollarwar11@gmail.com>
2024-08-11 10:15:54 +0530

    seperator explaination

---
+++ seperator.py
@@ -0,0 +1,24 @@
+a="hellow"+"world" #both are string function
+print(a)
+a="hellow" +"10" 
+print(a)
+print("hellow","30+20.6j")
+#if you want to seperate the value into the variable can use 
+#seperate,#end=" "
+a,b,c=10,20,30 
+print(a,b,c,sep='|') 
+print(a,b,c,sep='/') 
+a,b,c= 10,20,30
+print(a,b,c,sep=",")
+print(a,b,c,sep="/")
+a,b,c,d,e="yash","learning","python","in","softronix"
+print(a,b,c,d,e,sep="/")
+a="yash dollarwar"
+b=25
+c="python"
+d="softronix"
+print("age of",a,"is",b)
+print()
+print("yash learning",c,"in",d)
+
+
