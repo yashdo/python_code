369ed99634cdf0f04924922d0b8d9691eef043ca
yash dollarawar <yashdollarwar11@gmail.com>
2024-08-11 10:09:23 +0530

    reverse string code

---
+++ reversestring.py
@@ -0,0 +1,48 @@
+s="yash learning python in softronix"
+s1=s.replace("python","java")
+print(s1)
+s="ababababababababaababbabab"
+s2=s.replace("b","a")
+print(s2)
+#split will split your string value by given the seign
+h="nagpur,is/one/of/the_best_city"
+i=h.split('/')
+print(i)
+#split reverse join
+s="yash learning python in softronix"
+l=s.split( )
+r=l[::-1]
+j1=''.join(r)
+print(j1)
+s1="yash is the student of sotronix"
+s2=s1.split()
+s3=s2[::-1]
+s4=''.join(s3)
+print(s4)
+s3="yash is learning python is softronix"
+s4=s3.split()
+s5=s4[::-1]
+s6=''.join(s5)
+print(s6)
+s1="yash is-the-king-of-python"
+s2=s1.split("-")
+print(s2)
+s1="yash join indian neavy in march"
+s2=''.join(s1)
+print(s2)
+y="yash wants to become an  officer in indian aarmy "
+y1=y.split()
+y2=y1[::-1]
+y3=''.join(y2)
+print(y3)
+s="abababaabababababababab"
+s1=s.replace("b","a")
+print(s1)
+s="abababaabababababababab"
+s2="ba"
+print(s2.find(s2))
+s3=len(s)  #
+s=-1
+while True:
+    sop=(s2.find(s),n)
+
