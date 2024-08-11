af16731f355c308cf549319cf14c21a2d1eb0fa2
yash dollarawar <yashdollarwar11@gmail.com>
2024-08-11 10:17:48 +0530

    slice operator

---
+++ slice.py
@@ -0,0 +1,8 @@
+#slicing
+#forward and backword
+#forward:-slice[bigin:end-1:steps]
+#backword :-slice[bigin:end+1:steps]
+s="softronix"
+print(s[1:-1]) #s[1:-1-1] #oftr
+print(s[1:-5000])  #s[1:-5000] #blank
+print(s[1:-5000:-1])  #print full string
